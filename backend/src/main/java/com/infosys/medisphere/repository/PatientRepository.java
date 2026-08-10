c9b4d01111e00c940a92e4fdf287ad9ed4ac1246
AKSHAYA R <itsakshaya.ravi@gmail.com>
2026-08-10 18:29:26 +0530

    Delete backend directory

--- backend/src/main/java/com/infosys/medisphere/repository/PatientRepository.java
+++
@@ -1,9 +0,0 @@
-package com.infosys.medisphere.repository;
-
-import com.infosys.medisphere.model.PatientTwin;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PatientRepository extends MongoRepository<PatientTwin, String> {
-}
